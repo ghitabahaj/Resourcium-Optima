d3b52f37e5efbd5dccefbd827024cd36b810d446
Ghita <bahajghita3@gmail.com>
2023-10-11 16:08:48 +0100

    Employee Entity created

---
+++ src/main/java/com/youcode/resourcium/Entities/Employee.java
@@ -0,0 +1,54 @@
+package com.youcode.resourcium.Entities;
+
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Employee {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "position")
+    private String position;
+
+
+    public Employee(Long id, String username, String password, String firstName, String lastName, String email, String position) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", position='" + position + '\'' +
+                '}';
+    }
+}
