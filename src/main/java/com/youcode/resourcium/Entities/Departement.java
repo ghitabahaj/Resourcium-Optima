e0868697b65db026e2c16d9a112d480665568dd9
Ghita <bahajghita3@gmail.com>
2023-10-12 11:14:32 +0100

    Create the other classes

---
+++ src/main/java/com/youcode/resourcium/Entities/Departement.java
@@ -0,0 +1,4 @@
+package com.youcode.resourcium.Entities;
+
+public class Departement {
+}
--- src/main/java/com/youcode/resourcium/Entities/Employee.java
+++ src/main/java/com/youcode/resourcium/Entities/Employee.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import java.util.Date;
 
 @Entity
+@Table(name = "employee")
 public class Employee {
 
 
